e5b39ee6303c864319e93240d14120969fdf0ff5
abdellatif <abdelatif.yabahddou@gmail.com>
2026-09-25 15:07:12 +0000

    addCandidate for one and multiples

--- main.js
+++ main.js
@@ -18,30 +18,15 @@ let choice = Number(p("Enter a number between 1 and 3: "))
 
 switch(choice){
     case 1:
-         let CIN = p("write your CIN: ");
-         let firstname = p("enter your firstname: ");
-         let lastname = p("enter your lastname: ")
-         let political_party = p("enter your political party: ")
-         let age = p("enter your age: ")
-
-         if (!CIN || !firstname || !lastname || !political_party || isNaN(age)) {
-                console.log("please fill in all the fields");
-                break;
-            }
-        const newCandidate = {
-            CIN : CIN,
-            firstname: firstname,
-            lastname: lastname,
-            political_party: political_party,
-            age: age
-        }
-        candidates.push(newCandidate);
+        addCandidate(candidates)    
         console.log("Done");
-        
         break;
 
     case 2:
-        console.log("not good");
+        let number = Number(p("How many candidates? "));
+        for (let i = 0; i < number; i++) {
+            addCandidate(candidates);
+        }
         break;
     
     case 3:
@@ -53,3 +38,43 @@ switch(choice){
         break;  
 }
 
+function addCandidate(candidates){
+    let CIN = p("write your CIN: ");
+    let firstname = p("enter your firstname: ");
+    let lastname = p("enter your lastname: ")
+    let political_party = p("enter your political party: ")
+    let age = p("enter your age: ")
+
+    if (!CIN || !firstname || !lastname || !political_party || isNaN(age)) {
+        console.log("please fill in all the fields");
+        return;
+    }
+
+    if (age < 21) {
+        console.log("sorry, You can't be a candidate");
+        return;
+    }
+
+    let exists = false;
+        for (let i = 0; i < candidates.length; i++){
+            if (candidates[i].CIN === CIN) {
+                exists = true;
+                break;
+            }
+        }
+    if (exists) {
+        console.log("This CIN already exists.");
+        return;
+    }
+            
+    const newCandidate = {
+        CIN : CIN,
+        firstname: firstname,
+        lastname: lastname,
+        political_party: political_party,
+        age: age,
+        voters: []
+        }
+        
+    candidates.push(newCandidate);;
+}
